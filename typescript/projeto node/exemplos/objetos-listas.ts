c0513931549ac0616a83d1e9247ab2dfcf2d8a96
deniseomelo <nissemello@gmail.com>
2022-08-01 14:09:49 -0300

    Sumary

---
+++ typescript/projeto node/exemplos/objetos-listas.ts
@@ -0,0 +1,73 @@
+const pessoa = {
+    nome:'Denise',
+    idade: 28,
+    profissao:"desenvolvedora"
+}
+
+pessoa.idade = 25;
+
+const andre: (nome: string, idade: number, profissao: string) =>{
+    nome: 'Andre',
+    idade: 25,
+    profissao: 'pintor'
+}
+
+const paula: (nome: string, idade: number, profissao: string) => {
+    nome: 'Paula',
+    idade: 25,
+    profissao: 'Desenvolvedora'
+
+}
+
+enum Profissao {
+    Professora,
+    Atriz,
+    Desenvolvedora,
+    JogadoraDeFutebol
+}
+
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao?: Profissao
+
+}
+
+interface Estudante extends Pessoa {
+    materias: string[]
+}
+
+const vanessa: Pessoa = {
+    nome: 'Vanessa',
+    idade: 23,
+    profissao: Profissao.Desenvolvedora
+}
+
+const maria: Pessoa - {
+    nome: 'Maria', 
+    idade: 23,
+    Profissao: Profissao.Desenvolvedora
+}
+
+const jessica: Estudante = {
+    nome: 'Jessica',
+    idade:28,
+    profissao: Profissao.Desenvolvedora,
+    materias: ['Matematica discreta', 'programacâo']
+}
+
+const monica: Estudante = {
+    nome: 'Jessica',
+    idade:28,
+    materias: ['Matematica discreta', 'programacâo']
+
+}
+
+function listar(lista: string[]) {
+    for (const item of lista) {
+        console.log('-',item);
+    }
+
+}
+
+listar(monica.materias);
